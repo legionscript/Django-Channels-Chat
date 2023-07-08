cf20e3665002aa61e38f2d4b038f66e6c918cd93
Legion Script <58012792+legionscript@users.noreply.github.com>
2023-07-07 23:03:53 -0400

    Update settings.py

--- chat/settings.py
+++ chat/settings.py
@@ -148,7 +148,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
